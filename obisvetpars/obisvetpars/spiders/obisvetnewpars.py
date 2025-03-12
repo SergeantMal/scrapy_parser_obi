7ec0762e553d144b4719f981857f6f6dca9e6bff
SergeantMal <dersergeant@gmail.com>
2025-03-12 16:22:01 +0300

    add pagination

--- obisvetpars/obisvetpars/spiders/obisvetnewpars.py
+++ obisvetpars/obisvetpars/spiders/obisvetnewpars.py
@@ -7,8 +7,12 @@ class ObisvetnewparsSpider(scrapy.Spider):
     start_urls = ["https://obi.ru/osveschenie"]
 
     def parse(self, response):
-        svetilniki = response.css('div.FuS7R')
-        for svetilnik in svetilniki:
+        # Ограничиваем область парсинга товарами ДО блока с популярными товарами
+        product_section = response.css('div._3yNpa div.FuS7R')
+
+
+
+        for svetilnik in product_section:
             name = svetilnik.css('p._1UlGi::text').get()
             price = svetilnik.css('div._1YQve span::text').get()
             link = svetilnik.css('a').attrib['href']
@@ -22,3 +26,12 @@ class ObisvetnewparsSpider(scrapy.Spider):
                 'price': price,
                 'link': 'https://obi.ru' + link,
             }
+
+        # Пагинация: проверяем, есть ли следующая страница
+        current_page = response.url.split("page=")[-1] if "page=" in response.url else "1"
+        current_page = int(current_page)
+
+        if current_page < 69:  # Лимит страниц
+            next_page = f"https://obi.ru/osveschenie?page={current_page + 1}"
+            yield response.follow(next_page, callback=self.parse)
+
